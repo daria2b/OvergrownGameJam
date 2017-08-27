d94d27a59e3065cd743bc6a9e9df9955f327cbd2
Daria2b <d.bagina@gmail.com>
2017-08-27 18:17:13 -0400

    Changed implementation of grabber script

--- GrabberScript.cs
+++ GrabberScript.cs
@@ -5,44 +5,40 @@ using System.Collections;
 public class GrabberScript : MonoBehaviour {
 
 	public bool isGrabbed;
-	RaycastHit2D hit;
+	GameObject grabbed;
 	//maximum distance at which we can grab something
-	public float distance = 1f;
+	public float distance = 5f;
 	//where the grabbed object will be placed oiin the player
 	public Transform holdPoint;
 	public float throwForce;
 
 
-	void Update () {
-		//when a player clicks mouse button on object
-		if (Input.GetKeyDown (KeyCode.E)) {
-			//if object is not grabbed, then we want to grab it
-			if (!isGrabbed) {
-				//to make sure the collider doesn't start at the start of the player character and does not collide with itself
-				Physics2D.queriesStartInColliders = false;
-				//throw a raycast in that direction, to the right. Raycasts return a raycast hit
-				hit = Physics2D.Raycast (transform.position, Vector2.right * transform.localScale.x, distance);
-
-				//check if the raycast hit something
-				if (hit.collider != null && hit.collider.tag == "Grabbable") {
+	void OnTriggerStay2D (Collider2D other) {
+		if (other.gameObject.tag == "Grabbable") {
+			if (Input.GetKeyDown (KeyCode.E) || Input.GetMouseButtonDown (0)) {
+				//if object is not grabbed, then we want to grab it
+				if (!isGrabbed) {
+					grabbed = other.gameObject;
 					isGrabbed = true;
 				}			
 			}
-		//if an object is grabbed, then we want to throw it
-		//only throw an object if it doesn not collide with anything (such as terrain)
-			else {
+		}
+	}
+
+	void Update () {
+		//this will put the grabbed object to the position of a holdPoint game object attached to the player
+		if (isGrabbed && grabbed != null) {
+			grabbed.transform.position = holdPoint.position;
+				
+			if (Input.GetKeyDown (KeyCode.R) || Input.GetMouseButtonDown (1)) {
 				isGrabbed = false;
 				//to be able to throww an object, we need to make sure it has a rigidbody attached to it
-				if (hit.collider.gameObject.GetComponent<Rigidbody2D> () != null) {
-					//object will be thrown in direction of the mouse
-					hit.collider.gameObject.GetComponent<Rigidbody2D> ().velocity = new Vector2 (transform.localScale.x, 1) * throwForce;
-				
+				if (grabbed.GetComponent<Rigidbody2D> () != null) {
+					//object will be thrown at 45 degrees angle
+					grabbed.GetComponent<Rigidbody2D> ().velocity = new Vector2 (transform.localScale.x, 1) * throwForce;
 				}
 			}
 		}
-			//this will put the grabbed object to the position of a holdPoint game object attached to the player
-			if (isGrabbed)
-				hit.collider.gameObject.transform.position = holdPoint.position;
-	
 	}
+		
 }
