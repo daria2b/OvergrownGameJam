56d53dd3e6ae0ee41bcf4b809d4ea2ffa5a6c8d7
Daria2b <d.bagina@gmail.com>
2017-08-28 18:46:56 -0400

    Initial script files

--- RabbitController.cs
+++ RabbitController.cs
@@ -10,9 +10,13 @@ public class RabbitController : MonoBehaviour {
 	public Slider hungerSlider;
 
 	public static bool full;
+	public GameObject crumblesFX;
+
+	Animator myAnim;
 
 	// Use this for initialization
 	void Start () {
+		myAnim = GetComponent<Animator> ();
 		full = false;	
 		currentHunger = 0;
 		hungerSlider.maxValue = maxHunger;
@@ -29,8 +33,14 @@ public class RabbitController : MonoBehaviour {
 	void OnTriggerEnter2D (Collider2D other) {
 		if (other.tag == "Grabbable") {
 			currentHunger += 1f;
+			myAnim.SetTrigger ("foodArrived");
 			hungerSlider.value = currentHunger;
 			Destroy (other.gameObject);
+			Invoke ("LaunchCrumblesFX", 0.3f);
 		}
+	}
+
+	void LaunchCrumblesFX () {
+		Instantiate (crumblesFX, transform.Find("head").transform.position, transform.rotation);
 	}
 }
